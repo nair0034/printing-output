ee08f140eed0abbee334fedbae0583e3d4c0b4ad
nair0034 <122370026+nair0034@users.noreply.github.com>
2023-01-17 09:32:22 -0500

    js added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
+//initial setup
 
+let a = 3;
+let b = 5;
+let c;
+
+let output = 'let a = 3;\n';
+output += 'let b = 5;\n';
+output += 'let c;\n';
+output += '----------\n';
+output += ('a + b =' + (a + b));
+
+alert(output);
