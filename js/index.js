e7c640e1ef688a72beab021b8dd2e9ddb7552543
nair0034 <122370026+nair0034@users.noreply.github.com>
2023-01-17 09:57:43 -0500

    task 1 done

--- js/index.js
+++ js/index.js
@@ -8,6 +8,22 @@ let output = 'let a = 3;\n';
 output += 'let b = 5;\n';
 output += 'let c;\n';
 output += '----------\n';
-output += ('a + b =' + (a + b));
+output += ('a + b = ' + (a + b) + "\n");
+output += ('a - b = ' + (a - b) + "\n");
+output += ('a * b = ' + (a * b) + "\n");
+output += ('a / b = ' + (a / b) + "\n");
+output += ('a % b = ' + (a % b) + "\n");
+output += ('a += b = ' + (a += b) + "\n");
+output += ('a -= b = ' + (a -= b) + "\n");
+output += ('a *= b = ' + (a *= b) + "\n");
+output += ('a /= b = ' + (a /= b) + "\n");
+output += ('a %= b = ' + (a %= b) + "\n");
+output += ('a == b = ' + (a == b) + "\n"); 
+output += ('a != b = ' + (a != b) + "\n");
+output += ('a > b = ' + (a > b) + "\n");
+output += ('a < b = ' + (a < b) + "\n");
+output += ('!a && !c = ' + (!a && !c) + "\n");
+output += ('!a || !c  = ' + (!a || !c) + "\n");
+
 
 alert(output);
