50243b9c450db249af983de2c38bb0b9bacba35a
nair0034 <122370026+nair0034@users.noreply.github.com>
2023-01-24 15:32:07 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+let a = 3;
+let b = 5;
+let c;
+
+    let output = 'let a = 3;\n';
+    output += 'let b = 5;\n';
+    output += 'let c;\n';
+    output += '-----------\n';
+    output += 'a + b = ' + (a + b) + "\n";
+    output += 'a - b = ' + (a - b) + "\n";
+    output += 'a * b = ' + (a * b) + "\n";
+    output += 'a / b = ' + (a / b) + "\n";
+    output += 'a % b = ' + (a % b) + "\n";
+    output += 'a += b = ' + (a += b) + "\n";
+    output += 'a -= b = ' + (a -= b) + "\n";
+    output += 'a *= b = ' + (a *= b) + "\n";
+    output += 'a /= b = ' + (a /= b) + "\n";
+    output += 'a %= b = ' + (a %= b) + "\n";
+    output += 'a == b = ' + (a == b) + "\n";
+    output += 'a != b = ' + (a != b) + "\n";
+    output += 'a > b = ' + (a > b) + "\n";
+    output += 'a < b = ' + (a < b) + "\n";
+    output += '!a && !c = ' + (!a && !c) + "\n";
+    output += '!a || !c = ' + (!a || !c) + "\n";
+
+    alert(output);
+
+let first_name = "Rahul";
+let last_name = "Nair";
+let email = "nair0034@algonquinlive.com";
+
+
+    output = 'My name is '
+    output += first_name;
+    output += ' ';
+    output += last_name;
+    output += '.'
+    output += ' ';
+    output += 'You can contact me at ';
+    output += email;
+    output += '.'
+    
+    alert(output);
