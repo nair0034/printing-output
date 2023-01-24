dd385cad58beef2af8931b72fc5aeeb3b92df9da
nair0034 <122370026+nair0034@users.noreply.github.com>
2023-01-23 20:06:05 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,37 +1,58 @@
 //initial setup
-
 let a = 3;
 let b = 5;
 let c;
 
-let output = 'let a = 3;\n';
-output += 'let b = 5;\n';
-output += 'let c;\n';
-output += '----------\n';
-output += ('a + b = ' + (a + b) + "\n");
-output += ('a - b = ' + (a - b) + "\n");
-output += ('a * b = ' + (a * b) + "\n");
-output += ('a / b = ' + (a / b) + "\n");
-output += ('a % b = ' + (a % b) + "\n");
-output += ('a += b = ' + (a += b) + "\n");
-output += ('a -= b = ' + (a -= b) + "\n");
-output += ('a *= b = ' + (a *= b) + "\n");
-output += ('a /= b = ' + (a /= b) + "\n");
-output += ('a %= b = ' + (a %= b) + "\n");
-output += ('a == b = ' + (a == b) + "\n"); 
-output += ('a != b = ' + (a != b) + "\n");
-output += ('a > b = ' + (a > b) + "\n");
-output += ('a < b = ' + (a < b) + "\n");
-output += ('!a && !c = ' + (!a && !c) + "\n");
-output += ('!a || !c  = ' + (!a || !c) + "\n");
+let Output = 'let a = 3;\n';
+Output += 'let b = 5;\n';
+Output += 'let c;\n';
+Output += '----------\n';
+Output += ('a + b = ' + (a + b) + "\n");
+Output += ('a - b = ' + (a - b) + "\n");
+Output += ('a * b = ' + (a * b) + "\n");
+Output += ('a / b = ' + (a / b) + "\n");
+Output += ('a % b = ' + (a % b) + "\n");
+Output += ('a += b = ' + (a += b) + "\n");
+Output += ('a -= b = ' + (a -= b) + "\n");
+Output += ('a *= b = ' + (a *= b) + "\n");
+Output += ('a /= b = ' + (a /= b) + "\n");
+Output += ('a %= b = ' + (a %= b) + "\n");
+Output += ('a == b = ' + (a == b) + "\n"); 
+Output += ('a != b = ' + (a != b) + "\n");
+Output += ('a > b = ' + (a > b) + "\n");
+Output += ('a < b = ' + (a < b) + "\n");
+Output += ('!a && !c = ' + (!a && !c) + "\n");
+Output += ('!a || !c  = ' + (!a || !c) + "\n");
+
+alert(Output);
 
 
 
 
 
 
+
+
+let first_name = "Rahul";
+let last_name = "Nair";
+let email = "rnair070@gmail.com";
+let output;
+
+
+
+
+output = '"My name is ' 
+output += first_name;
+output += ' ';
+output += last_name;
+output += '.';
+output += ' You can contact me at ' 
+output += email;
+output += '."';
 
 alert(output);
+
+
 
 
 
